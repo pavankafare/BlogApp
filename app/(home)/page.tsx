7f05435badec17b201ea4ce9fda5f9c38a1cba9d
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-02-20 02:51:40 +0530

    HeroSection done

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
