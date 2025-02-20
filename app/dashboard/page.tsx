238806445250f14ec457d42747ce2319e27767c1
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-02-20 20:15:42 +0530

    Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import BlogDashboard from "@/components/dashboard/blog-dashboard";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <BlogDashboard />;
+};
 
-export default page
+export default page;
