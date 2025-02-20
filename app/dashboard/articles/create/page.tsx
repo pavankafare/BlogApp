31ac1acb2a615260d67d14d427172e0ca2d7de8b
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2025-02-20 20:38:30 +0530

    Create Articles Page

---
+++ app/dashboard/articles/create/page.tsx
@@ -0,0 +1,8 @@
+import CreateArticlePage from "@/components/articles/create-articles-page";
+import React from "react";
+
+const page = () => {
+  return <CreateArticlePage />;
+};
+
+export default page;
